ea10d356bdadf81544d87699d6d706b246d05e1b
lijian <lijian@topscomm.com>
2021-02-22 15:55:53 +0800

    first commit

---
+++ 打地鼠/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace 打地鼠
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Clock = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Clock
+            // 
+            this.Clock.Enabled = true;
+            this.Clock.Interval = 10;
+            this.Clock.Tick += new System.EventHandler(this.Clock_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 351);
+            this.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "打地鼠";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Clock;
+    }
+}
+
---
+++ 打地鼠/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Media;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace 打地鼠
+{
+    public partial class MainForm : Form
+    {
+        PictureBox[] Mouse = new PictureBox[9];
+        int Count = 80;
+        int Position = 10;
+        bool Appear = false;
+        public MainForm()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 9; i++)
+            {
+                Mouse[i] = new PictureBox();
+                Mouse[i].Location = new Point(120 * (i % 3), 117 * (i / 3));
+                Mouse[i].Size = new Size(120, 117);
+                Mouse[i].Tag = i;
+                Mouse[i].Cursor = new Cursor(new Bitmap(Properties.Resources.Hammer).GetHicon());
+                Mouse[i].Click += new EventHandler(Mouse_Click);
+                Mouse[i].Image = Properties.Resources.Mouse1;
+                Controls.Add(Mouse[i]);
+            }
+        }
+        private void Mouse_Click(object sender, EventArgs e)
+        {
+            int i = (int)((PictureBox)sender).Tag;
+            if (i == Position)
+            {
+                new SoundPlayer(Properties.Resources.Point).Play();
+                Mouse[i].Image = Properties.Resources.Mouse1;
+                Position = 10;
+                Count = 80;
+                Appear = false;
+            }
+        }
+        private void Clock_Tick(object sender, EventArgs e)
+        {
+            if (!Appear)
+            {
+                if (++Count > 100)
+                {
+                    Count = 0;
+                    while (true)
+                    {
+                        int i = new Random().Next() % 9;
+                        if (i!= Position)
+                        {
+                            Mouse[i].Image = Properties.Resources.Mouse2;
+                            Appear = true;
+                            Position = i;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
